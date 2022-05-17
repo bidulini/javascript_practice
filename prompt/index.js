e13fa2e07eb67d0f41bf94ef33afa07748bb9a0d
bidulini <abidabida@hotmail.co.uk>
2022-05-17 20:31:54 +0200

    Added BeginnerFunctions Folder.

---
+++ prompt/index.js
@@ -0,0 +1,10 @@
+var a=prompt("Unesite broj:");
+a=parseInt(a);
+b=a+5;
+console.log("a =", a);
+console.log("b = a+5 =", b);
+
+var godina=prompt("Unesite godinu rodjenja: ")
+var mesec=prompt("Unesite dan i mesec rodjenja: ");
+
+console.log("Vi ste rodjeni godine:",godina,"dan i mesec:",mesec);
